b32aae1e3391dfa14c3676ffea34f0e9430d3847
Dominik <dominik_lazarecki@wp.pl>
2023-01-24 13:35:39 +0100

    ButtonFetchUsers.js change

--- src/components/ButtonFetchUsers.js
+++ src/components/ButtonFetchUsers.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const ButtonFetchUsers = props => {
-    return (
-        <button style={{
-            padding: '10px 20px',
-            border: '2px solid black',
-            backgroundColor: 'white',
-            margin: 20,
-        }} onClick={props.onClick}>Dodaj 5 użytkowników</button>
-    );
-}
- 
+const ButtonFetchUsers = (props) => {
+  return (
+    <button
+      style={{
+        padding: "10px 20px",
+        border: "2px solid black",
+        backgroundColor: "white",
+        margin: 20,
+      }}
+      onClick={props.onClick}
+    >
+      Dodaj użytkownika
+    </button>
+  );
+};
+
 export default ButtonFetchUsers;
